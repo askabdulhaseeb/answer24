efa4c064547972436aba81195fddd239b4ce90c3
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2023-04-11 03:58:35 +0500

    sign in UI + Url luncher + asstes

---
+++ lib/utilities/utilities.dart
@@ -0,0 +1,4 @@
+class Utilities {
+  static const String webURL = 'https://www.answer24.nl';
+  static const String signUpURL = 'https://www.answer24.nl/pricing';
+}
--- lib/views/auth/signin_screen.dart
+++ lib/views/auth/signin_screen.dart
@@ -1,10 +1,25 @@
+import 'package:answer24/functions/url_funcation.dart';
 import 'package:answer24/utilities/app_image.dart';
+import 'package:answer24/widgets/custom/custom_elevated_button.dart';
+import 'package:answer24/widgets/custom/custom_textformfield.dart';
+import 'package:answer24/widgets/custom/password_textformfield.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+import '../../utilities/utilities.dart';
+
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
   static const String routeName = '/signin';
 
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +35,55 @@ class SignInScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
               child: Image.asset(AppImages.logo),
             ),
+            CustomTextFormField(
+              controller: _email,
+              title: 'Email',
+              hint: 'username@mail.com',
+            ),
+            PasswordTextFormField(controller: _password),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Forget Password?',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            CustomElevatedButton(
+              title: 'Sign In',
+              onTap: () {},
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Theme.of(context).disabledColor),
+                  children: [
+                    const TextSpan(text: '''Don't have account? '''),
+                    TextSpan(
+                      text: 'Register here',
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          try {
+                            await UrlFuncation()
+                                .lunchStringURL(Utilities.signUpURL);
+                          } catch (e) {
+                            if (kDebugMode) {
+                              print('SignIn View - Error: ${e.toString()}');
+                            }
+                          }
+                        },
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
